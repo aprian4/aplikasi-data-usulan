2e0b02bd5c148a2761bcc63099b0a221e73a1d5a
aprian4 <aprian.cendekia@gmail.com>
2021-06-16 00:40:05 +0700

    edit laporan

--- application/views/laporan/index.php
+++ application/views/laporan/index.php
@@ -24,7 +24,40 @@
                         </center>
                     </div>
                     <div class="card-body">
-                        LAPORAN
+                        <div class="container justify-content-center mt-5">
+                            <form>
+                                <div class="form-group row">
+                                    <label class="col-sm-2">Jenis Layanan</label>
+                                    <select class="col-sm-8 custom-select" name='jenis_usulan'>
+                                        <option value="all" selected>-- Semua --</option>
+                                        <option value="karpeg">Kartu Pegawai (KARPEG)</option>
+                                        <option value="karsu">Kartu Suami (KARSU)</option>
+                                        <option value="karis">Kartu Istri (KARIS)</option>
+                                        <option value="idcard">Kartu Tanda Pengenal (ID CARD)</option>
+                                        <option value="cuti">Cuti</option>
+                                    </select>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-sm-2">Jenis Usulan</label>
+                                    <select class="custom-select col-sm-8" name='jenis_usulan'>
+                                        <option value="all" selected>-- Semua --</option>
+                                        <option value="karpeg_baru">Baru</option>
+                                        <option value="karpeg_pengganti">Pengganti Karena Hilang</option>
+                                    </select>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-sm-2">Tanggal Awal</label>
+                                    <input type="date" class="form-control col-sm-8" name="tgl_awal">
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-sm-2">Tanggal Akhir</label>
+                                    <input type="date" class="form-control col-sm-8" name="tgl_akhir">
+                                </div>
+                                <div class="form-group text-center mt-5">
+                                    <button type="submit" class="btn btn-primary"><i class="fas fa-download"> Unduh Laporan</i></button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
